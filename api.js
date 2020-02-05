90afa68b707420875da2dc3906fe82ab8145243d
mishelpa <judith.paredesartica@gmail.com>
2020-02-05 03:19:24 -0500

    Cambiar ruta de archivo

--- api.js
+++ api.js
@@ -3,10 +3,7 @@
 
 const newLink = require('./index');
 
-newLink.mdLinks('.', { validate: false })
+newLink.mdLinks('/home/mishel/Desktop/Laboratoria/LIM011-fe-md-links/src/testFolder/read.md', { validate: true })
   .then((data) => {
     console.log(data);
   });
-
-
-console.log(process.argv);
